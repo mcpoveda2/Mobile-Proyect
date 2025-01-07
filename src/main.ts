9c5ec8ae1bcbd549845288fa9aa607598f2d13ae
Luis F. Romero <382-lfromero@users.noreply.gitlab.espol.edu.ec>
2025-01-06 20:53:07 -0500

    Cambios comentados por probelmas con el firebase

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
-import { firebaseConfig } from './credentials'; 
+//import { firebaseConfig } from './credentials'; 
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
@@ -14,7 +14,7 @@ bootstrapApplication(AppComponent, {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+  //  provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideFirestore(() => getFirestore()),
   ],
 });
