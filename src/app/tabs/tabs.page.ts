60dddd6dfa851b6b4cf6613a69f63b97d850665f
Luis F. Romero <lromeror@fiec.espol.edu.ec>
2025-01-12 09:46:54 -0500

    Cambios en al interfaz general Tabs

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,20 +1,39 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
-import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import { 
+  IonTabs, 
+  IonTabBar, 
+  IonTabButton, 
+  IonIcon, 
+  IonLabel, 
+  IonFabButton, 
+  IonHeader, 
+  IonToolbar, 
+  IonTitle 
+} from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { triangle, ellipse, square, peopleCircle,clipboardOutline} from 'ionicons/icons';
-
+import { homeOutline, personOutline, add } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
   standalone: true,
-  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
+  imports: [
+    IonTabs, 
+    IonTabBar, 
+    IonTabButton, 
+    IonIcon, 
+    IonLabel, 
+    IonFabButton, 
+    IonHeader, 
+    IonToolbar, 
+    IonTitle
+  ],
 })
 export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ clipboardOutline, triangle, ellipse, square, peopleCircle});
+    addIcons({ homeOutline, personOutline, add });
   }
 }
