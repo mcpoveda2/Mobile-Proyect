560b7047391118c2602180a3773c64e69e32af9f
Luis F. Romero <lromeror@fiec.espol.edu.ec>
2025-01-12 18:30:18 -0500

    Cambios en los cards con repectiva categoria

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -11,7 +11,8 @@ import {
   IonCardSubtitle, 
   IonCardTitle, 
   IonSelect, 
-  IonSelectOption 
+  IonSelectOption, 
+  IonChip 
 } from '@ionic/angular/standalone';
 import { CommonModule } from '@angular/common';
 
@@ -34,10 +35,11 @@ import { CommonModule } from '@angular/common';
     IonCardTitle,
     IonSelect,
     IonSelectOption,
+    IonChip, 
   ],
 })
 export class Tab1Page {
-  public userName: string = 'Anna'; // Nombre del usuario
+  public userName: string = 'Luis Romero'; // Nombre del usuario
   public profilePicture: string = 'assets/images/profile-picture.jpeg'; // Ruta de la imagen del perfil
 
   public cards = [
@@ -46,12 +48,14 @@ export class Tab1Page {
       subtitle: '10 May',
       content: `Today was awesome! Went to Skagen with Petra and John. 
         Skagen is so calm and relaxing city, first we saw the city and then we went to the beach...`,
+      tags: ['Happy', 'Sunny', 'Trip', 'Skagen'],
     },
     {
       title: 'Birthday Party',
       subtitle: '7 May',
       content: `Today I was invited to Lucas birthday party, he’s gonna have birthday in 6 days. 
         I need to buy him some gift. Need to write down some ideas. The day was usual...`,
+      tags: ['Boring', 'Rainy', 'Friends'],
     },
     {
       title: 'Vet Visit',
@@ -59,15 +63,16 @@ export class Tab1Page {
       content: `I woke up, ate breakfast and made some coffee. 
         The day started as usual until I found out that my Max is sick! 
         I had to take him to the vet. I was really worried...`,
+      tags: ['Worried', 'Cloudy', 'Max', 'Vet', 'Pet'],
     },
   ];
-
+  
   public sortOptions = [
     { label: 'Date', value: 'date' },
     { label: 'Title', value: 'title' },
   ];
 
-  public sortedCards = [...this.cards]; // Tarjetas ordenadas
+  public sortedCards = [...this.cards]; 
 
   constructor() {}
 
