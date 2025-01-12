93cf8c0e08293fd21f1a663410db95b01b4067a1
Luis F. Romero <lromeror@fiec.espol.edu.ec>
2025-01-12 01:22:57 -0500

    Cambios de prueba en APP

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,14 +1,58 @@
 import { Component } from '@angular/core';
-import {  IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import {
+  IonCard,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCardContent,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonIcon,
+  IonRange,
+  IonButton,
+  IonLabel,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent,],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonCardContent,
+    IonIcon,
+    IonRange,
+    IonButton,
+    IonLabel,
+  ],
 })
 export class Tab3Page {
-  constructor() {}
+  emotions = [
+    { label: 'Very Sad', icon: 'sad-outline' },
+    { label: 'Sad', icon: 'frown-outline' },
+    { label: 'Neutral', icon: 'ellipse-outline' },
+    { label: 'Happy', icon: 'happy-outline' },
+    { label: 'Very Happy', icon: 'smile-outline' },
+  ];
+
+  currentEmotion = this.emotions[3]; // Default: Happy
+
+  onEmotionChange(event: any) {
+    this.currentEmotion = this.emotions[event.detail.value];
+  }
+
+  onContinue() {
+    console.log('Emotion selected:', this.currentEmotion);
+    // Lógica adicional para manejar el botón "Continue"
+  }
 }
