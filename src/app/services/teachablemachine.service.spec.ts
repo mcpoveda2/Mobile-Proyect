f595d4bda74f83a811b4d1460b4f1c64a064269f
AlejandroSV2004 <alesvera@espol.edu.ec>
2025-01-20 08:48:24 -0500

    Teachable machine

---
+++ src/app/services/teachablemachine.service.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed } from '@angular/core/testing';
+
+import { TeachablemachineService } from './teachablemachine.service';
+
+describe('TeachablemachineService', () => {
+  let service: TeachablemachineService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(TeachablemachineService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
+
+
